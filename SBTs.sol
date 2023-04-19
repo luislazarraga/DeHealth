aa1a8a601fdbca4270fa4b3752d0857562a743cd
luislazarraga <l.perezlazarraga@usp.ceu.es>
2023-04-19 21:31:29 +0200

    Corrección Vulnerabilidades 1 en SBT

--- SBTs.sol
+++ SBTs.sol
@@ -1,25 +1,38 @@
 pragma solidity ^0.8.0;
 
-contract SBT {
-    struct PersonalData {
-        string name;
-        string surname;
-        string country;
-        // Otros atributos relevantes para la identidad de la persona
+contract SBTCode {
+    mapping(address => uint256) balances;
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(recipient != address(0), "SBT: transfer to zero address");
+        require(amount > 0, "SBT: transfer amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "SBT: insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
     }
     
-    mapping (address => PersonalData) private sbtData;
-    mapping (address => bool) private sbtExists;
-    
-    function createSBT(string memory name, string memory surname, string memory country) public {
-        require(!sbtExists[msg.sender], "Ya existe un SBT para esta dirección");
-        sbtData[msg.sender] = PersonalData(name, surname, country);
-        sbtExists[msg.sender] = true;
+    function isValidAddress(address addr) internal view returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size == 0 && addr != 0;
     }
     
-    function getSBTData(address sbtAddress) public view returns (string memory, string memory, string memory) {
-        require(sbtExists[sbtAddress], "No existe un SBT para esta dirección");
-        PersonalData memory data = sbtData[sbtAddress];
-        return (data.name, data.surname, data.country);
+    function transferSecurely(address recipient, uint256 amount, bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) external returns (bool) {
+        require(isValidAddress(recipient), "SBT: invalid recipient address");
+        require(amount > 0, "SBT: transfer amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "SBT: insufficient balance");
+        require(ecrecover(messageHash, v, r, s) == sbtauthorizer, "SBT: invalid authorization");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
     }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
