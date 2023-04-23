3510e6231b1d8e641341575e8c9a23e965304a33
luislazarraga <l.perezlazarraga@usp.ceu.es>
2023-04-23 16:21:20 +0200

    SBTCode Remastered - Añadida función requestSBT(). El objetivo es permitir a los usuarios obtener su SBT de forma segura

--- SBTs.sol
+++ SBTs.sol
@@ -7,14 +7,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SBTCode is ERC721, Ownable, IERC5484 {
     uint256 private _currentTokenId = 0;
     mapping (uint256 => BurnAuth) private _burnAuth;
+    mapping (address => bool) private _hasSBT;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
+    function requestSBT() external returns (uint256) {
+        require(!_hasSBT[msg.sender], "You already have an SBT");
+        _currentTokenId++;
+        uint256 newTokenId = _currentTokenId;
+        _safeMint(msg.sender, newTokenId);
+        _burnAuth[newTokenId] = BurnAuth(msg.sender, address(0));
+        _hasSBT[msg.sender] = true;
+
+        emit Issued(owner(), msg.sender, newTokenId, _burnAuth[newTokenId]);
+
+        return newTokenId;
+    }
+
     function issue(address to, BurnAuth burnAuth) external onlyOwner returns (uint256) {
+        require(!_hasSBT[to], "User already has an SBT");
         _currentTokenId++;
         uint256 newTokenId = _currentTokenId;
         _safeMint(to, newTokenId);
         _burnAuth[newTokenId] = burnAuth;
+        _hasSBT[to] = true;
 
         emit Issued(owner(), to, newTokenId, burnAuth);
 
@@ -25,5 +41,10 @@ contract SBTCode is ERC721, Ownable, IERC5484 {
         require(_exists(tokenId), "Token does not exist");
         return _burnAuth[tokenId];
     }
+
+    function hasSBT(address user) external view returns (bool) {
+        return _hasSBT[user];
+    }
 }
+
 
