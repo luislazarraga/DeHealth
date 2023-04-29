8ac648943bca743e9e2586452195816bd6899d2f
luislazarraga <l.perezlazarraga@usp.ceu.es>
2023-04-29 18:47:52 +0200

    Depurada la clase CajaFuerte salud. Eliminados requires y mappings innecesarios. Eliminada la función del signer (al menos temporalmente) para garantías de seguridad.

--- MedicalVault.sol
+++ MedicalVault.sol
@@ -26,33 +26,22 @@ contract CajaFuerteSalud {
         string situacionActual;
     }
 
-    mapping(address => bool) private hasVault;
     mapping(uint256 => Registro[]) private cajaFuerteRegistros;
 
-    function crearRegistroEnCajaFuerte(string memory condicionesSalud, string memory registroMedico, string memory medicacion, string memory situacionActual, uint256 _soulBoundToken) public hasSBT(_soulBoundToken) {
-        require(!hasVault[msg.sender], "The user has not registered their medical record yet");
+    function annadirRegistroEnCajaFuerte(string memory condicionesSalud, string memory registroMedico, string memory medicacion, string memory situacionActual, uint256 _soulBoundToken) public hasSBT(_soulBoundToken) {
         Registro memory registro = Registro(condicionesSalud, registroMedico, medicacion, situacionActual);
-        cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)].push(registro);
-        hasVault[msg.sender] = true;
+        cajaFuerteRegistros[_soulBoundToken].push(registro);
     }
 
-
     function recuperarRegistro(uint256 index, uint256 _soulBoundToken) public view hasSBT(_soulBoundToken) returns (string memory, string memory, string memory, string memory)  {
-        require(cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)].length > 0, "No hay registros medicos de este paciente");
-        Registro memory registro = cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)][index];
+        require(cajaFuerteRegistros[_soulBoundToken].length > 0, "No hay registros medicos de este paciente");
+        Registro memory registro = cajaFuerteRegistros[_soulBoundToken][index];
         return (registro.condicionesSalud, registro.registroMedico, registro.medicamentos, registro.situacionActual);
     }
 
     function recuperarVault(uint256 _soulBoundToken) public view hasSBT(_soulBoundToken) returns (Registro[] memory) {
-        require(cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)].length > 0, "No hay registros medicos de este paciente");
-        Registro[] memory registros = cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)];
-        return registros;
-    }
-
-    function recuperarVaultAjeno(uint256 _soulBoundToken) public view hasSBT(_soulBoundToken){
-        require(cajaFuerteRegistros[_soulBoundToken].length > 0, "No hay registros medicos de este paciente");
-        require(sbtContract.amIFam(_soulBoundToken), "El duenno de este SBT no confia en ti");
-         Registro[] memory registros = cajaFuerteRegistros[sbtContract.walletOfOwner(msg.sender)];
+        require(cajaFuerteRegistros[_soulBoundToken].length > 0, "No hay registros medicos de este paciente");        
+        Registro[] memory registros = cajaFuerteRegistros[_soulBoundToken];
         return registros;
     }
 
@@ -61,36 +50,8 @@ contract CajaFuerteSalud {
         _;
     }
 
-    modifier friendSBT(uint256 _trusterSBT) {
-        
-        _;
-    }
-
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this operation");
         _;
     }
-
-    function recoverSigner(bytes32 _messageHash, bytes memory _signature) internal pure returns (address) {
-        require(_signature.length == 65, "Invalid signature length");
-
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        assembly {
-            r := mload(add(_signature, 0x20))
-            s := mload(add(_signature, 0x40))
-            v := byte(0, mload(add(_signature, 0x60)))
-        }
-
-        if (v < 27) {
-            v += 27;
-        }
-
-        require(v == 27 || v == 28, "Invalid signature value");
-
-        return ecrecover(_messageHash, v, r, s);
-    }
 }
- 
