d960864a29a0bf2272f28efc261a438db0ebbd11
luislazarraga <l.perezlazarraga@usp.ceu.es>
2023-06-05 18:30:44 +0200

    Pequeñas correcciones en el codigo de SBTCode ante posibles malas ejecuciones

--- SBTs.sol
+++ SBTs.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC5484.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
@@ -17,11 +16,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SBTCode is ERC721Enumerable, IERC5484 {
-    uint256 private _currentTokenId;
-    mapping(uint256 => BurnAuth) private _burnAuth;
+contract SBTCode is ERC721Enumerable {
+    uint256 private _currentTokenId = 0;
 
-    mapping(uint256 => address[]) private _SBTApprovals;
+    mapping(uint256 => address[]) public _SBTApprovals;
 
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
@@ -29,39 +27,26 @@ contract SBTCode is ERC721Enumerable, IERC5484 {
 
     function issue() external returns (uint256) {
         require(balanceOf(msg.sender) < 1, "Solo puedes tener un SBT");
-        ++_currentTokenId;
         uint256 newTokenId = _currentTokenId;
+        _currentTokenId++;
         _safeMint(msg.sender, newTokenId);
-        _burnAuth[newTokenId] = BurnAuth.OwnerOnly;
         return newTokenId;
     }
 
-    function burnAuth(uint256 tokenId)
-        external
-        view
-        override
-        returns (BurnAuth)
-    {
-        require(_exists(tokenId), "Token does not exist");
-        return _burnAuth[tokenId];
-    }
-
     function walletOfOwner(address _owner) public view returns (uint256) {
-        return tokenOfOwnerByIndex(_owner, 0);
+        uint256 _tokenId = tokenOfOwnerByIndex(_owner, 0);
+        return _tokenId;
     }
 
-    function amIFam(uint256 trusterSBT) public view returns (bool) {
+    function amIFam(uint256 trusterSBT) public view returns (address) {
         //  if (trusterSBT ) token = truster wallet = trusted. Comprueba si pa un token tu direccion está trusteada
         // ejecuta el trusted
-        for (uint256 i; i <= getApprovedSBT(trusterSBT).length;) {
+        for (uint256 i; i < getApprovedSBT(trusterSBT).length; ++i) {
             if (msg.sender == getApprovedSBT(trusterSBT)[i]) {
-                return true;
-            }
-            unchecked {
-                ++i;
+                return msg.sender;
             }
         }
-        return false;
+        return address(0);
     }
 
     function _beforeTokenTransfer(
@@ -92,11 +77,8 @@ contract SBTCode is ERC721Enumerable, IERC5484 {
         public
         hasSBT(_soulBoundToken)
     {
-        for (uint256 i; i < getApprovedSBT(_soulBoundToken).length;) {
+        for (uint256 i = 0; i < getApprovedSBT(_soulBoundToken).length; i++) {
             _SBTApprovals[walletOfOwner(msg.sender)].pop();
-            unchecked {
-                ++i;
-            }
         }
     }
 
@@ -109,18 +91,14 @@ contract SBTCode is ERC721Enumerable, IERC5484 {
             _SBTApprovals[walletOfOwner(msg.sender)].length <= 2,
             "Has concecido ya los dos permisos extraordinarios"
         );
-        for (uint256 i=1; i<=_SBTApprovals[walletOfOwner(msg.sender)].length;)
+        for (uint256 i; i< _SBTApprovals[walletOfOwner(msg.sender)].length; i++)
         {
             if (_SBTApprovals[walletOfOwner(msg.sender)][i] == _yourFriend)
             {
                 return false;
             }
-            unchecked {
-                ++i;
-            }
         }
         approveSBT(_yourFriend, walletOfOwner(msg.sender));
-        //_SBTApprovals[walletOfOwner(msg.sender)].push(_yourFriend);
         return true;
     }
 
